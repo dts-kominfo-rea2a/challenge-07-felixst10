8ae789f4e92927f28478d08d4f4dceaf2c39d23f
felixst10 <theofelix501@gmail.com>
2022-06-12 22:55:57 +0700

    push promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// TODO: Buat fungsi promiseOutput sesuai ketentuan
+const promiseOutput = async (penonton) => {
+  try{
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+
+    const jumlahMarahAtauTidak = dataIXX.filter((allData) => {
+      return allData.hasil == penonton;
+    }).length +
+    dataVGC.filter((allData) => {
+      return allData.hasil == penonton;
+    }).length;
+
+    return jumlahMarahAtauTidak;
+  } catch (err) 
+  {console.log(err)};
+}
 
 module.exports = {
   promiseOutput,
